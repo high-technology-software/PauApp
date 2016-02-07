f49e60e40e92a9b5e8c03b8c8893bd738955c3ff
Elrick81 <christianalos@Christians-MacBook-Air.local>
2016-02-07 23:19:14 +0100

    comentario prueba

--- app/src/main/java/com/high_technology_software/android/pauapp/MainActivity.java
+++ app/src/main/java/com/high_technology_software/android/pauapp/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //hola como va??
     }
 
     @Override
